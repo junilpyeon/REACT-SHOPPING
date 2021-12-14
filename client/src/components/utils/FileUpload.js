1cb07f98ec0fb211f465eebd1e2fce9a8381b5da
junilpyeon <dreamerjunil@gmail.com>
2021-12-14 16:23:19 +0900

    uploadpage 1.1

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -64,7 +64,7 @@ function FileUpload(props) {
             <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
 
                 {Images.map((image, index) => (
-                    <div onClick={() => onDelete(image)}>
+                    <div onClick={() => onDelete(image)}> /*onClick delete*/
                         <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
                     </div>
                 ))}
